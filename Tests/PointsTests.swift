4422babd1ad15924830836ce407f1efc54771260
Felix Gabel <felix.gabel@me.com>
2016-01-20 19:25:00 +0100

    Add Points.swift

---
+++ Tests/PointsTests.swift
@@ -0,0 +1,27 @@
+
+import XCTest
+import UI
+
+class PointsTests: XCTestCase {
+
+	static let accuracy = Points(0.0000001)
+
+	// MARK: -
+
+	func test_clamp() {
+		XCTAssertEqualWithAccuracy(Points(13.051).clamp(), 1.0, accuracy:PointsTests.accuracy)
+		XCTAssertEqualWithAccuracy(Points(-3.999).clamp(), 0.0, accuracy:PointsTests.accuracy)
+	}
+
+	func test_clampWithMin() {
+		XCTAssertEqualWithAccuracy(Points(-3.999).clamp(min:3.5), 3.5, accuracy:PointsTests.accuracy)
+	}
+
+	func test_clampWithMax() {
+		XCTAssertEqualWithAccuracy(Points(99.99).clamp(max:13.13), 13.13, accuracy:PointsTests.accuracy)
+	}
+
+    func test_clampWithEqual() {
+        XCTAssertEqualWithAccuracy(Points(1.0).clamp(), 1.0, accuracy:PointsTests.accuracy)
+    }
+}
