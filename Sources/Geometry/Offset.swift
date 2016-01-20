c55db6ee43378d88bddc6ee299391a2b54b85bbc
Felix Gabel <felix.gabel@me.com>
2016-01-21 00:56:55 +0100

    Add real Offset.swift

---
+++ Sources/Geometry/Offset.swift
@@ -0,0 +1,32 @@
+
+public struct Offset : GeometryType, CustomStringConvertible {
+
+    public static let zero:Offset = 0.0
+
+    // MARK: -
+
+    public var horizontal:Points
+    public var vertical:Points
+
+    // MARK: -
+
+    public init(horizontal:Points = 0.0, vertical:Points = 0.0) {
+        (self.horizontal, self.vertical) = (horizontal, vertical)
+    }
+
+    public init(floatLiteral value:Points) {
+        self.init(horizontal:value, vertical:value)
+    }
+
+    // MARK: - CustomStringConvertible
+
+    public var description:String {
+        return "Offset(horizontal:\(horizontal), vertical:\(vertical))"
+    }
+}
+
+// MARK: - Operators
+
+public func == (left:Offset, right:Offset) -> Bool {
+    return (left.horizontal == right.horizontal && left.vertical == right.vertical)
+}
