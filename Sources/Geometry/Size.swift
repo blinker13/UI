7f6fc5d50aab486bc2ad4f2a36c6853222b64e85
Felix Gabel <felix.gabel@me.com>
2016-01-20 20:25:44 +0100

    Add Size.swift

---
+++ Sources/Geometry/Size.swift
@@ -0,0 +1,37 @@
+
+public struct Size : GeometryType, CustomStringConvertible {
+
+    public static let zero:Size = 0.0
+
+	// MARK: -
+
+	public var min:Points
+	public var max:Points
+
+	// MARK: -
+
+	public init(min:Points = 0.0, max:Points = .infinity) {
+		precondition(min <= max, "Size can not be negative")
+		(self.min, self.max) = (min, max)
+	}
+	
+	public init(floatLiteral value:Points) {
+		self.init(min:value, max:value)
+	}
+
+	// MARK: - CustomStringConvertible
+
+	public var description:String {
+		return "Size(min:\(min), max:\(max))"
+	}
+}
+
+// MARK: - Operators
+
+public func == (left:Size, right:Size) -> Bool {
+	return (left.min == right.min && left.max == right.max)
+}
+
+public func ... (start:Points, end:Points) -> Size {
+	return Size(min:start, max:end)
+}
