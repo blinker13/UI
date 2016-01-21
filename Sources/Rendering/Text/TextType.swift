170a41c3a58087f3fd073e6a2b10ce59cdc0f0c5
Felix Gabel <felix.gabel@me.com>
2016-01-22 00:23:42 +0100

    Simplify TextType protocol

--- Sources/Rendering/Text/TextType.swift
+++ Sources/Rendering/Text/TextType.swift
@@ -1,33 +1,6 @@
 
-public protocol TextType : CollectionType, StringLiteralConvertible {
-	subscript (index:String.Index) -> Glyph { get }
+public protocol TextType {
+	subscript(index:String.Index) -> Glyph { get }
 	var content:String { get }
 	init(content:String)
-}
-
-// MARK: -
-
-extension TextType {
-
-	public init(unicodeScalarLiteral value:StringLiteralType) {
-		self.init(content:"\(value)")
-	}
-
-	public init(extendedGraphemeClusterLiteral value:String) {
-		self.init(content:value)
-	}
-
-	public init(stringLiteral value:String) {
-		self.init(content:value)
-	}
-
-	// MARK: -
-
-	public var startIndex:String.Index {
-		return content.startIndex
-	}
-
-	public var endIndex:String.Index {
-		return content.endIndex
-	}
 }
