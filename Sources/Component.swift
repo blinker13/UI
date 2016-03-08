6d1398504e3a3d89fcb9fe33ba2aa65a0fccb7e3
Felix Gabel <felix.gabel@me.com>
2016-03-08 23:56:37 +0100

    Add Component extensions

--- Sources/Component.swift
+++ Sources/Component.swift
@@ -5,3 +5,16 @@ public protocol Component {
 	var width:Dimension { get }
 	var margin:Space { get }
 }
+
+// MARK: -
+
+extension Component {
+	public final var minBoxWidth:Unit { return width.min + margin.right + margin.left }
+	public final var maxBoxWidth:Unit { return width.max + margin.right + margin.left }
+
+	public final var minBoxHeight:Unit { return height.min + margin.top + margin.bottom }
+	public final var maxBoxHeight:Unit { return height.max + margin.top + margin.bottom }
+
+	public final var minBoxSize:Size { return Size(minBoxWidth, minBoxHeight) }
+	public final var maxBoxSize:Size { return Size(maxBoxWidth, maxBoxHeight) }
+}
