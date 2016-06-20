772593817ef15ae575c5d432580c9bcda9462f77
Felix Gabel <felix.gabel@me.com>
2016-06-20 18:58:42 +0200

    Update example with iPad support

--- Examples/iOS/iOS/AppDelegate.swift
+++ Examples/iOS/iOS/AppDelegate.swift
@@ -6,53 +6,49 @@ import UI
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window:UIWindow?
+	var window:UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        let s0 = Style(
-            height:Length(min:70),
-            background:.red,
-            width:40.0
-        )
+		let bar = Style(
+			height:44.0
+		)
 
-        let s1 = Style(
-            alignment:.center,
-            width:Length(min:80),
-            height:Length(min:10, max:50),
-            border:Border(radius:5, width:2),
-            background:.yellow,
-            margin:5.0
-        )
+		let s0 = Style(
+			background:.red
+		)
 
-        let s2 = Style(
-            alignment:.trailing,
-            width:Length(min:150),
-            shadow:Shadow(offset:3.0),
-            background:.green,
-            height:40.0
-        )
+		let s1 = Style(
+			alignment:.center,
+			width:Length(min:80),
+			border:Border(radius:5, width:2),
+			background:.yellow,
+			margin:20.0
+		)
 
-        let cs = Style(
-            arrangement:.horizontal,
-            distribution:.order,
-            background:.white,
-            padding:5.0
-        )
-        
-        let container = View(style:cs,
-            View(style:s0),
-            View(style:s1),
-            View(style:s2)
-        )
+		let s2 = Style(
+			background:.green
+		)
 
-        let screen = UIScreen.main()
-        let aWindow = UIWindow(frame:screen.bounds)
-        aWindow.rootViewController = QuartzViewController(component:container)
-        aWindow.makeKeyAndVisible()
-        window = aWindow
+		let cs = Style(
+			padding:Space(top:20.0),
+			distribution:.order,
+			background:.gray
+		)
+		
+		let container = View(style:cs,
+			View(style:[s0, bar]),
+			View(style:s1),
+			View(style:[s2, bar])
+		)
 
-        return true
-    }
+		let screen = UIScreen.main()
+		let aWindow = UIWindow(frame:screen.bounds)
+		aWindow.rootViewController = QuartzViewController(component:container)
+		aWindow.makeKeyAndVisible()
+		window = aWindow
+
+		return true
+	}
 }
 
