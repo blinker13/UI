822c63686464e0374c288d0c1d936684e75508d7
Felix Gabel <felix.gabel@me.com>
2016-07-01 23:43:07 +0200

    Update iOS example

--- Examples/iOS/iOS/AppDelegate.swift
+++ Examples/iOS/iOS/AppDelegate.swift
@@ -8,43 +8,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window:UIWindow?
 
+	let bar = Style(
+		height:44.0
+	)
+
+	let navigation = Style(
+		background:.red
+	)
+
+	let contentStyle = Style(
+		alignment:.center,
+		width:Dimensions(min:80),
+		border:Border(radius:5, width:2),
+		background:.yellow,
+		margin:20.0
+	)
+
+	let tool = Style(
+		background:.green
+	)
+
+	let root = Style(
+		padding:Space(top:20.0),
+		distribution:.order,
+		background:.white
+	)
+
+	var content:Component {
+		return View(style:root,
+			View(style:[navigation, bar]),
+			View(style:contentStyle),
+			View(style:[tool, bar])
+		)
+	}
+
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
-		let bar = Style(
-			height:44.0
-		)
-
-		let s0 = Style(
-			background:.red
-		)
-
-		let s1 = Style(
-			alignment:.center,
-			width:Dimensions(min:80),
-			border:Border(radius:5, width:2),
-			background:.yellow,
-			margin:20.0
-		)
-
-		let s2 = Style(
-			background:.green
-		)
-
-		let cs = Style(
-			padding:Space(top:20.0),
-			distribution:.order,
-			background:.gray
-		)
-		
-		let container = View(style:cs,
-			View(style:[s0, bar]),
-			View(style:s1),
-			View(style:[s2, bar])
-		)
 
 		let screen = UIScreen.main()
 		let aWindow = UIWindow(frame:screen.bounds)
-		aWindow.rootViewController = QuartzViewController(component:container)
+		aWindow.rootViewController = QuartzViewController(component:content)
 		aWindow.makeKeyAndVisible()
 		window = aWindow
 
