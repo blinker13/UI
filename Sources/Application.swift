1d41beab2ce019c9e9da688f916ef202b89e7534
Felix Gabel <felix.gabel@me.com>
2017-05-13 16:29:25 +0900

    ⭐️ Add Application.name and .path properties

--- Sources/Application.swift
+++ Sources/Application.swift
@@ -3,6 +3,16 @@ public final class Application {
 
 	public static let shared = Application()
 
+	var name:String {
+		let sequence = path.characters.split(separator:"/")
+		let components = sequence.map(String.init)
+		return components.last!
+	}
+
+	var path:String {
+		return CommandLine.arguments.first!
+	}
+
 	internal var window:Window?
 }
 
