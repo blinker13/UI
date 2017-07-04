e9405737ed8b444a9420aebb9152bd0775099765
Felix Gabel <felix.gabel@me.com>
2017-07-04 20:46:39 +0900

    💄 Clean up Scene.swift

--- Sources/Scene+Rendering.swift
+++ Sources/Scene+Rendering.swift
@@ -1,50 +1,5 @@
 
-public final class Scene {
-
-	internal let root:Node
-	internal var nodes:Set<Node>
-
-	internal init (with node:Node) {
-		self.nodes = [node]
-		self.root = node
-	}
-}
-
-extension Scene : Page {
-
-	public var width:Dimensions { return root.component.width }
-	public var height:Dimensions { return root.component.height }
-
-	public func onStart() { page?.onStart() }
-	public func onResume() { page?.onResume() }
-	public func onPause() { page?.onPause() }
-	public func onStop() { page?.onStop() }
-
-	public func render(with context: Context) -> [Component] {
-		return [root.component]
-	}
-}
-
-public extension Scene {
-	convenience init (with component:Component) {
-		let node = Node(with:component)
-		self.init(with:node)
-	}
-}
-
 internal extension Scene {
-
-	func update(_ size:Size) {
-		if size == root.frame.size { return }
-		root.frame.size = size
-		mark(root)
-	}
-
-	func mark(_ node:Node) {
-		let parent = node.parent ?? node
-		nodes.insert(parent)
-	}
-
 	func display(with renderer:Renderer) {
 		for node in AnyIterator(nextNode) {
 			display(node, with:renderer)
@@ -53,10 +8,6 @@ internal extension Scene {
 }
 
 private extension Scene {
-
-	var page:Page? {
-		return root.component as? Page
-	}
 
 	func nextNode() -> Node? {
 		guard let node = nodes.first else { return nil }
