5f4b30753a8abbda0bd7f45048187d1234b3e1d8
Felix Gabel <felix.gabel@me.com>
2019-03-04 19:03:26 +0900

    💄 Improve Node model rendering
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Sources/Runtime/Element.swift
+++ Sources/Runtime/Element.swift
@@ -2,25 +2,3 @@
 public protocol Element : Encodable {
 
 }
-
-// MARK: -
-
-internal extension Element {
-
-	func render() -> Children {
-		switch component?.render() {
-		case .some(let all as Children): return all
-		case .some(let child): return [child]
-		default: return []
-		}
-	}
-}
-
-// MARK: -
-
-private extension Element {
-
-	var component:Component? {
-		return self as? Component
-	}
-}
--- Sources/Runtime/Node.swift
+++ Sources/Runtime/Node.swift
@@ -33,7 +33,7 @@ public extension Node {
 internal extension Node {
 
 	func mount() {
-		let elements = model.render()
+		let elements = render()
 		let nodes = elements.map(realize)
 		nodes.forEach { $0.mount() }
 		children = nodes
@@ -54,5 +54,17 @@ private extension Node {
 		let node = Node(with:element)
 		node.parent = self
 		return node
+	}
+
+	func render() -> Children {
+
+		switch model {
+			case let children as Children: return children
+			case let component as Component: switch component.render() {
+				case let children as Children: return children
+				case let child: return [child]
+			}
+			default: return []
+		}
 	}
 }
