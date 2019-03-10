cf7fb3a40ee92a51ea27789b4c879d9215764b82
Felix Gabel <felix.gabel@me.com>
2019-03-10 23:43:16 +0900

    🔨 extend Array<Element> conforming to Component
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Sources/Component.swift
+++ Sources/Component.swift
@@ -2,3 +2,9 @@
 public protocol Component : Element {
 	func render() -> Element
 }
+
+// MARK: -
+
+extension Array : Component where Element == UI.Element {
+	public func render() -> Element { return self }
+}
