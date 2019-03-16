c8d8395459c63c86c236f26dd9b7b5fb0ce42aaa
Felix Gabel <felix.gabel@me.com>
2019-03-17 00:57:16 +0900

    🔨 sepparate Fragment file in Utility group
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Sources/Component.swift
+++ Sources/Component.swift
@@ -2,9 +2,3 @@
 public protocol Component : Element {
 	func render() -> Element
 }
-
-// MARK: -
-
-extension Array : Component where Element == UI.Element {
-	public func render() -> Element { return self }
-}
