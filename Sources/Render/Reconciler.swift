4f293bc813a27580a97266ce78ef2fa18aa35e0c
Felix Gabel <felix.gabel@me.com>
2019-03-17 01:04:15 +0900

    🔨 add Reconciler implementation
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

---
+++ Sources/Render/Reconciler.swift
@@ -0,0 +1,59 @@
+
+internal struct Reconciler {
+
+	private typealias Cache = [Key:Node]
+
+	fileprivate struct Key : Hashable {
+		private let alias:Alias
+		private let scope:Scope
+		private let index:Int
+	}
+
+	private var cache:Cache
+}
+
+// MARK: -
+
+internal extension Reconciler {
+
+	var remains:[Node] {
+		return Array(cache.values)
+	}
+
+	init (with children:Node.Children) {
+		let enumeration = children.enumerated()
+		let initial = Cache(minimumCapacity:children.count)
+		self.cache = enumeration.reduce(into:initial) {
+			let key = Key(with:$1.element, at:$1.offset)
+			return $0[key] = $1.element
+		}
+	}
+
+	mutating func match(_ element:Element, at index:Int) -> Node? {
+		let key = Key(with:element, at:index)
+		return cache.removeValue(forKey:key)
+	}
+}
+
+// MARK: -
+
+private extension Reconciler.Key {
+
+	static func == (left:Reconciler.Key, right:Reconciler.Key) -> Bool {
+		switch (left.scope, right.scope) {
+		case (.none, .none): return left.index == right.index
+		case (.key(let l), .key(let r)): return l == r
+		default: return false
+		}
+	}
+
+	var hashValue:Int {
+		return alias.hashValue
+	}
+
+	init (with element:Element, at index:Int) {
+		self.alias = element.alias
+		self.scope = element.scope
+		self.index = index
+	}
+}
